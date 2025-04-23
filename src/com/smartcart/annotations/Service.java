6ef72eeb4e6b6cb4a8883b2f389001c939a0373d
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-04-23 23:58:04 +0530

    Update Service.java

--- src/com/smartcart/annotations/Service.java
+++ src/com/smartcart/annotations/Service.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Custom annotation to mark service classes for dynamic instantiation.
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Service {
